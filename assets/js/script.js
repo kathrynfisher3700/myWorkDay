8c36cbd7def4571095ca94b8cd68aaff46a07eb2
Aprecity <kathrynfisher3700@gmail.com>
2023-10-05 21:21:54 -0400

    added comments to js

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,64 +1,43 @@
-// Wrap all code that interacts with the DOM in a call to jQuery to ensure that
-// the code isn't run until the browser has finished rendering all the elements
-// in the html.
+//_.~"~._.~"~._.~"~._.~"~.__.~"~._.~"~._.~"~._.~"~.__.~"~._.~"~._.~"~._.~"~._
+//Grabs current date from day.js and stores into element in header
 
-  // TODO: Add a listener for click events on the save button. This code should
-  // use the id in the containing time-block as a key to save the user input in
-  // local storage. HINT: What does `this` reference in the click listener
-  // function? How can DOM traversal be used to get the "hour-x" id of the
-  // time-block containing the button that was clicked? How might the id be
-  // useful when saving the description in local storage?
-  //
-  // TODO: Add code to apply the past, present, or future class to each time
-  // block by comparing the id to the current hour. HINTS: How can the id
-  // attribute of each time-block be used to conditionally add or remove the
-  // past, present, and future classes? How can Day.js be used to get the
-  // current hour in 24-hour time?
-  //
-  // TODO: Add code to get any user input that was saved in localStorage and set
-  // the values of the corresponding textarea elements. HINT: How can the id
-  // attribute of each time-block be used to do this?
-  //
-  // TODO: Add code to display the current date in the header of the page.
+let day = document.getElementById("currentDay"); //element
+let today = dayjs().format("MMMM D, YYYY"); //current day
+day.innerHTML = today; //adds current day to element
 
+//_.~"~._.~"~._.~"~._.~"~.__.~"~._.~"~._.~"~._.~"~.__.~"~._.~"~._.~"~._.~"~._
+//Grabs current hour from day.js
 
+let hour = dayjs().format("H"); //current hour (24 hour clock)
+console.log(hour); //log to check if working
+let hourNow = Number.parseFloat(hour); //turns string into number
+console.log(hourNow); //check
 
-///This is the date!
-let day = document.getElementById("currentDay");
-let today = dayjs().format("MMMM D, YYYY");
-day.innerHTML = today;
+//_.~"~._.~"~._.~"~._.~"~.__.~"~._.~"~._.~"~._.~"~.__.~"~._.~"~._.~"~._.~"~._
+//For loop to grab all values of hour boxes -- compares them to current hour
 
-//This is the hour
-let hour = dayjs().format("H");
-console.log(hour);
-let hourNow = Number.parseFloat(hour);
-console.log(hourNow);
-
-
-
-   for(let i=9; i <=17; i++){
-    if (hour > i){
-      $(`#hour-${i}`).addClass("past");
-   } else if (hour == i){
-      $(`#hour-${i}`).addClass("present");
-   } else {
-    $(`#hour-${i}`).addClass("future");
+for (let i = 9; i <= 17; i++) { //hour box range is 9-17
+  if (hour > i) {
+    $(`#hour-${i}`).addClass("past"); //if hour has past, turn box gray
+  } else if (hour == i) {
+    $(`#hour-${i}`).addClass("present"); //if hour current, turn box green
+  } else {
+    $(`#hour-${i}`).addClass("future"); //if hour in future, turn box green
   }
 }
+//_.~"~._.~"~._.~"~._.~"~.__.~"~._.~"~._.~"~._.~"~.__.~"~._.~"~._.~"~._.~"~._
+//Grabs save button element
 
-//grab button
 let saveBtn = document.getElementsByClassName("saveBtn");
-console.log(saveBtn); //worked
+console.log(saveBtn); //check
 
-//grab typed info
+//Grabs value in text boxes
 let userText = document.querySelectorAll("description");
 let userProject = {
-    text: userText.value
-}
-//add event listener
-$("textarea").click(function(){
+  text: userText.value,
+};
+//Trying to add to local storage ---- more to learn but will update soon
+//does not throw error
+$("textarea").click(function () {
   localStorage.setItem("userProject", JSON.stringify(userProject));
 });
-
-
-  
